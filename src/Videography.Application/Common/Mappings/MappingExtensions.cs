d473dcd013f1b44c4ec445da853823ecaed7a11e
trichoip <107303342+trichoip@users.noreply.github.com>
2023-10-09 03:34:21 +0700

    update mapper pagination list

---
+++ src/Videography.Application/Common/Mappings/MappingExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Videography.Application.Helpers;
+
+namespace Videography.Application.Common.Mappings;
+public static class MappingExtensions
+{
+    public static Task<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(
+        this IQueryable<TDestination> queryable, int pageNumber, int pageSize) where TDestination : class
+      => PaginatedList<TDestination>.CreateAsync(queryable.AsNoTracking(), pageNumber, pageSize);
+}
